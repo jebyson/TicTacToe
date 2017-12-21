62adaed39fc1d4651e9aea3296e0d5125db2d213
Ebyson, Joshua <30394310+jebyson@users.noreply.github.com>
2017-12-21 17:18:32 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,31 +2,37 @@ package com.company;
 
 import java.util.*;
 
+import static com.company.boardState.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("This is the Tic Tac Toe board:");
+   public static void main(String[] args) {
+       System.out.println("This is the Tic Tac Toe board:");
 
-        new boardState();
+       new boardState();
 
-        boardState.printBoard();
-        Scanner input = new Scanner(System.in);
-        System.out.println("\nEnter '0' if you want to start. Enter '1' if you want the CPU to start.");
-        int whoGoesFirst = input.nextInt();
+       printBoard();
+       Scanner input = new Scanner(System.in);
+       System.out.println("\nEnter '0' if you want to start. Enter '1' if you want the CPU to start.");
+       int whoGoesFirst = input.nextInt();
 
-        if(whoGoesFirst == 0)
-        {
-            System.out.println("Too bad, the CPU is going first!");
-            boardState.CPUFirst();
-        }
-        else if(whoGoesFirst == 1)
-        {
-            System.out.println("The CPU will go first!");
-            boardState.CPUFirst();
-        }
-        else
-        {
-            System.out.println("Sorry, that is an invalid input. Goodbye.");
-        }
-    }
+       if(whoGoesFirst == 0)
+       {
+           System.out.println("Too bad, the CPU is going first!");
+
+           CPUFirst();
+
+           boardState.CPUFirst();
+
+       }
+       else if(whoGoesFirst == 1)
+       {
+           System.out.println("The CPU will go first!");
+           CPUFirst();
+       }
+       else
+       {
+           System.out.println("Sorry, that is an invalid input. Goodbye.");
+       }
+   }
 }
