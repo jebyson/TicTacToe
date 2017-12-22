826ed755997b723781cd73cd959c58436213f07b
Ebyson, Joshua <30394310+jebyson@users.noreply.github.com>
2017-12-22 08:17:20 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,28 +7,37 @@ import static com.company.boardState.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is the Tic Tac Toe board:");
+        for(int j = 0; j < 1; j +=0) {
+            System.out.println("This is the Tic Tac Toe board:");
 
-        new boardState();
+            new boardState();
 
-        printBoard();
-        Scanner input = new Scanner(System.in);
-        System.out.println("\nEnter '0' if you want to start. Enter '1' if you want the CPU to start.");
-        int whoGoesFirst = input.nextInt();
+            printBoard();
+            Scanner input = new Scanner(System.in);
+            System.out.println("\nEnter '0' if you want to start. Enter '1' if you want the CPU to start.");
+            int whoGoesFirst = input.nextInt();
 
-        if(whoGoesFirst == 0)
-        {
-            System.out.println("Too bad, the CPU is going first!");
-            CPUFirst();
-        }
-        else if(whoGoesFirst == 1)
-        {
-            System.out.println("The CPU will go first!");
-            CPUFirst();
-        }
-        else
-        {
-            System.out.println("Sorry, that is an invalid input. Goodbye.");
+            if (whoGoesFirst == 0)
+            {
+                System.out.println("Too bad, the CPU is going first!");
+                CPUFirst();
+            }
+            else if (whoGoesFirst == 1) {
+                System.out.println("The CPU will go first!");
+                CPUFirst();
+            }
+            else {
+                System.out.println("Sorry, that is an invalid input. Goodbye.");
+            }
+
+            System.out.println("\nIf you would like to play again, enter '0'.");
+            int playAgain = input.nextInt();
+
+            if(playAgain != 0)
+            {
+                System.out.println("Goodbye!");
+                break;
+            }
         }
     }
 }
