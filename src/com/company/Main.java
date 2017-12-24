28fa174a20cdea5bdb3a760fe9abd4d98edc7d20
Ebyson, Joshua <30394310+jebyson@users.noreply.github.com>
2017-12-24 14:58:30 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,47 +5,28 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        for(int j = 0; j < 1; j +=0) {
-            System.out.println("This is the Tic Tac Toe board:");
+        System.out.println("This is the Tic Tac Toe board:");
 
-            boardState tester = new boardState();
+        new boardState();
 
-            tester.printBoard();
-            Scanner input = new Scanner(System.in);
-            System.out.println("\nEnter '0' if you want to start. Enter '1' if you want the CPU to start.");
-            int whoGoesFirst = input.nextInt();
+        boardState.printBoard();
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nEnter '0' if you want to start. Enter '1' if you want the CPU to start.");
+        String whoGoesFirst = input.nextLine();
 
-            if (whoGoesFirst == 0)
-            {
-                System.out.println("Too bad, the CPU is going first!");
-                tester.CPUFirst();
-            }
-            else if (whoGoesFirst == 1) {
-                System.out.println("The CPU will go first!");
-                tester.CPUFirst();
-            }
-            else {
-                System.out.println("Sorry, that is an invalid input. Goodbye.");
-                break;
-            }
-
-            System.out.println("\nIf you would like to play again, enter '0'. If you would like to quit, enter '1'.");
-            int playAgain = input.nextInt();
-
-            if(playAgain == 0)
-            {
-                System.out.println("Thanks for playing again!\n");
-            }
-            else if(playAgain == 1)
-            {
-                System.out.println("Thanks for playing. Goodbye.");
-                break;
-            }
-            else
-            {
-                System.out.println("Sorry, that is an invalid input. Goodbye.");
-                break;
-            }
+        if(whoGoesFirst.equals("0"))
+        {
+            System.out.println("Too bad, the CPU is going first!");
+            boardState.CPUFirst();
+        }
+        else if (whoGoesFirst.equals("1"))
+        {
+            System.out.println("The CPU will go first!");
+            boardState.CPUFirst();
+        }
+        else
+        {
+            System.out.println("Sorry, that is an invalid input. Goodbye.");
         }
     }
 }
