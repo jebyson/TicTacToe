3804333e10053cf6ef95d1b99d1e03d570437899
Ebyson, Joshua <30394310+jebyson@users.noreply.github.com>
2018-01-10 18:52:16 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,28 +5,50 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is the Tic Tac Toe board:");
-
-        new boardState();
-
-        boardState.printBoard();
-        Scanner input = new Scanner(System.in);
-        System.out.println("\nEnter '0' if you want to start. Enter '1' if you want the CPU to start.");
-        String whoGoesFirst = input.nextLine();
-
-        if(whoGoesFirst.equals("0"))
+        for(int j = 0; j <= 1; j++)
         {
-            System.out.println("Too bad, the CPU is going first!");
-            boardState.CPUFirst();
-        }
-        else if (whoGoesFirst.equals("1"))
-        {
-            System.out.println("The CPU will go first!");
-            boardState.CPUFirst();
-        }
-        else
-        {
-            System.out.println("Sorry, that is an invalid input. Goodbye.");
+            System.out.println("This is the Tic Tac Toe board:");
+
+            boardState game = new boardState();
+
+            game.printBoard();
+            Scanner input = new Scanner(System.in);
+            System.out.println("\nEnter '0' if you want to start. Enter '1' if you want the CPU to start.");
+            String whoGoesFirst = input.nextLine();
+            whoGoesFirst = whoGoesFirst.trim();
+            int whoFirst = Integer.parseInt(whoGoesFirst);
+
+            if (whoFirst == 0)
+            {
+                System.out.println("Too bad, the CPU is going first!");
+                game.CPUFirst();
+            }
+            else if (whoFirst == 1)
+            {
+                System.out.println("The CPU will go first!");
+                game.CPUFirst();
+            }
+            else
+            {
+                System.out.println("Sorry, that is an invalid input. Goodbye.");
+            }
+
+            Scanner finalInput = new Scanner(System.in);
+            System.out.println("\nEnter '0' if you want to play again. Enter '1' if you want to quit.");
+            String playAgain = finalInput.nextLine();
+            playAgain = playAgain.trim();
+            int playOrNah = Integer.parseInt(playAgain);
+
+            if (playOrNah == 1)
+            {
+                j++;
+                System.out.println("Goodbye!");
+            }
+            else if (playOrNah != 0)
+            {
+                j++;
+                System.out.println("Invalid input. Goodbye!");
+            }
         }
     }
 }
