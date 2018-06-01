adba441228a1b9c94c49ad192f736a747bff57c5
Ebyson, Joshua <30394310+jebyson@users.noreply.github.com>
2018-06-01 08:42:31 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,12 @@
 package com.company;
 
-import java.util.*;
-
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("This is the Tic Tac Toe board:");
         new boardState();
         boardState.printBoard();
-        Scanner input = new Scanner(System.in);
-        System.out.println("\nEnter 'u' if you want to start. Enter 'c' if you want the CPU to start.");
-        String whoGoesFirst = input.nextLine();
-        String whoGoesFirstNow = whoGoesFirst.trim();
-
-        if (whoGoesFirstNow.equals("u")) {
-            System.out.println("Too bad, the CPU is going first!");
-            boardState.CPUFirst();
-        }
-        else if (whoGoesFirstNow.equals("c")) {
-            System.out.println("The CPU will go first!");
-            boardState.CPUFirst();
-        }
-        else {
-            System.out.println("Sorry, that is an invalid input. Goodbye.");
-        }
+        System.out.println("\nThe CPU will go first!");
+        boardState.CPUFirst();
     }
 }
